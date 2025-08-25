7f41727e096ac9f2572101178300f9a20b72dd0b
gabrielbaum <gabrielluizbaum@outlook.com>
2025-08-25 18:33:59 +0100

    novos testes

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`a velocidade de seu carro é ${vel} km/h`)
+if(vel>=60) 
+    { console.log(`Você ultrapassou a velocidade permitida. Multado !`)}
+console.log( `Dirija sempre usando cinto de segurança`)
+
